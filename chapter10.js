e5b999b0897649c90c9a9ad7b283a8bb44eab857
skilbjo <john.skilbeck@gmail.com>
2015-01-15 10:10:20 -0800

    Egg programming language

--- chapter10.js
+++ chapter10.js
@@ -1,1 +1,28 @@
 console.log(' === Modules === ');
+
+console.log('\n === Month Names === ');
+
+
+var weekDay = function() {
+	var names = ['Sunday','Monday','Tuesday',
+		'Wednesday','Thursday','Friday','Saturday'];
+  return {
+    name: function(number) { return names[number]; },
+    number: function(name) { return names.indexOf(name); }
+  };
+}(); 
+
+var monthNames = function() {
+	var names = ['January','February','March',
+		'April','May','June',
+		'July','August','September',
+		'October','November','December'];
+  return {
+    name: function(number) { return names[number]; },
+    number: function(name) { return names.indexOf(name); }
+  };
+}(); 
+
+console.log(monthNames.name(monthNames.number('December')));
+
+console.log('\n === Return to Electronic Life, 0% ===');
---
+++ chapter10a.js
@@ -0,0 +1,14 @@
+(function(exports) {
+	var names = ['Sunday','Monday','Tuesday',
+		'Wednesday','Thursday','Friday','Saturday'];
+
+	exports.name = function(number) {
+		return names[number];
+	};
+	exports.number = function(name) {
+		return names.indexOf(name);
+	};
+
+})(this.weekDay = {});
+
+console.log(weekDay.name(weekDay.number('Saturday')));
